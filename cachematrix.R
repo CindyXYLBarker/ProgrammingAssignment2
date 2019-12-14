6ff36bdc0c6dd371ed396a9b682e6ceaf6007436
Cindy Barker <Cindy_Barker@intuit.com>
2019-12-14 15:38:35 -0800

    Added implementation for matrix inversion caching support.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Utilities that provides caching support 
+## for Matrix inversion function
 
-## Write a short comment describing this function
-
+## Function that creates a special matrix object
+## that has cached inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) i <<- solve
+  getSolve <- function() i
+  list(set=set, get=get, setSolve=setSolve, 
+       getSolve=getSolve)
 }
 
-
-## Write a short comment describing this function
-
+## Function that implements caching support for
+## matrix inversion
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getSolve()
+  if (!is.null(i)){
+    message("getting cached version of inversion")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setSolve(i)
+  i
 }
